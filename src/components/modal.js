281e441549fb5502abd9f1175d6bfea7e1918e32
Vladislav Kuchma <vlad.kuchma.02@yandex.ru>
2022-09-11 13:05:58 +0300

    replace listeners for new card in card.js

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,3 +1,5 @@
+import { imagePopupDescription, imagePopupPicture } from './utils';
+
 export function closePopupByEsc(evt) {
   if (evt.key === 'Escape') {
     closePopup(document.querySelector('.popup_opened'));
@@ -20,4 +22,10 @@ export function openPopup(event, popup) {
   popup.classList.add('popup_opened');
 
   window.addEventListener('keydown', closePopupByEsc);
+};
+
+export function openImagePopup(event, popup){
+  imagePopupDescription.textContent = event.target.parentElement.querySelector('.element__title').textContent;
+  imagePopupPicture.src = event.target.parentElement.querySelector('.element__image').style.backgroundImage.slice(5,-2);
+  openPopup(event, popup);
 };
