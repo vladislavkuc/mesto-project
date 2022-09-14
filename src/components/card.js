deb6cd856906a8601b90c2b211590d4bfb0982b5
Vladislav Kuchma <vlad.kuchma.02@yandex.ru>
2022-09-14 05:40:53 +0300

    Add features for working with server. Commit before review

--- src/components/card.js
+++ src/components/card.js
@@ -1,25 +1,31 @@
 import { elementCard, imagePopup } from './utils';
 import { openImagePopup } from './modal';
+import { deleteCard, pressLikeHandler } from '../index';
 
-export function pressLike(event){
-  event.target.classList.toggle('element__button-like_active');
-};
-
-export function deleteCard(event){
-  event.target.parentElement.remove();
-};
-
-function setCardsListeners(card){
-  card.querySelector('.element__button-like').addEventListener('click', pressLike);
-  card.querySelector('.element__button-delete').addEventListener('click', deleteCard);
-  card.querySelector('.element__image').addEventListener('click', event => openImagePopup(event, imagePopup));
-  return card;
-}
-
-export function addCard(card) {
+export function addCard(card, profile) {
   const newCard = elementCard.querySelector('.element').cloneNode(true);
+  const deleteButton = newCard.querySelector('.element__button-delete');
+  const likeButton = newCard.querySelector('.element__button-like');
+
   newCard.querySelector('.element__image').style.backgroundImage = `url(${card.link})`;
   newCard.querySelector('.element__title').textContent = card.name;
+  newCard.querySelector('.element__likes-count').textContent = card.likes.length;
 
-  return setCardsListeners(newCard);
+  if (card.owner._id !== profile._id) {
+    newCard.removeChild(deleteButton);
+  } else {
+    deleteButton.addEventListener('click', () => deleteCard(card._id));
+  }
+
+
+  if (card.likes.map(like => JSON.stringify(like)).includes(JSON.stringify(profile))) {
+    likeButton.classList.add('element__button-like_active');
+  } else {
+    likeButton.classList.remove('element__button-like_active');
+  }
+
+  likeButton.addEventListener('click', evt => { pressLikeHandler(evt, card) }, {once : true});
+  newCard.querySelector('.element__image').addEventListener('click', event => openImagePopup(event, imagePopup));
+
+  return newCard;
 };
